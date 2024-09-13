ad657c803348c9f37f8a25fc4ae0f9c75ab5d4a0
Tofu-Xx <1714843907@qq.com>
2024-09-14 00:59:17 +0800

    chore: usevueuse

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
-const title = $ref(document.title);
-const editTitle = () => {
-  document.title = title;
-};
+const title = useTitle();
 </script>
 <template>
   <main font-sans p4 flex="~ col justify-center items-center gap2" text="center gray-700 dark:gray-200">
     <TheInput
       $="title"
       placeholder="typing in title"
-      autocomplete="false"
-      @input="editTitle" />
+      autocomplete="false" />
     <TheFooter />
   </main>
 </template>
