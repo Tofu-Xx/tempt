4447702dc278efcf5960977c65ef60e27a2ec141
Tofu-Xx <1714843907@qq.com>
2024-10-15 18:09:35 +0800

    chore: update

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -3,7 +3,10 @@ const title = useTitle()
 </script>
 
 <template>
-  <main font-sans p4 flex="~ col justify-center items-center gap2" text="center gray-700 dark:gray-200">
+  <main
+    font-sans p4
+    flex="~ col justify-center items-center gap2"
+    text="center gray-700 dark:gray-200">
     <TheInput
       ::="title"
       placeholder="typing in title"
